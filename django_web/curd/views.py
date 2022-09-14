6181f90f01733fe413d3908b837c34eadd49e1ec
renhaowx <renhao.wangx@intel.com>
2022-09-14 15:37:19 +0800

    add html css js img

--- django_web/curd/views.py
+++ django_web/curd/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "curd/index.html")
+
+
+def login(request):
+    return render(request, "curd/login.html")
+
