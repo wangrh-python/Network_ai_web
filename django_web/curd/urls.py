811a345ecb75b18280b8f204e64933aeba3319ae
renhaowx <renhao.wangx@intel.com>
2022-11-02 13:39:26 +0800

    add naal

--- django_web/curd/urls.py
+++ django_web/curd/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('chronos/performance/', views.chronos_performance, name='chronos_performance'),
     path('chronos/performance/view/', views.chronos_performance_view, name='chronos_performance_view'),
     path('chronos/accuracy/', views.chronos_accuracy, name='chronos_accuracy'),
-    path('chronos/accuracy/view', views.chronos_accuracy_view, name='chronos_accuracy_view'),
+    path('chronos/accuracy/view/', views.chronos_accuracy_view, name='chronos_accuracy_view'),
+    path('naal/performance/', views.naal_performance, name='naal_performance'),
+    path('naal/performance/view/', views.naal_performance_view, name='naal_performance_view'),
 
 ]
 
