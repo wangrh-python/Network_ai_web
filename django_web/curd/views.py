b071516d7656ec19f92fa9c95c9bd47a6bb0bd3d
junhao2x <junhaox.zhang@intel.com>
2022-11-24 17:47:44 +0800

    Update views.py

--- django_web/curd/views.py
+++ django_web/curd/views.py
@@ -111,32 +111,6 @@ def naal_performance_view(request):
     else:
             return HttpResponse('<h1>你在干什么</h1>')
 
-# 将excel数据写入mysql
-    def wrdb(filename):
-        # 打开上传 excel 表格
-        readbook = xlrd.open_workbook(settings.UPLOAD_ROOT + "/" + filename)
-        sheet = readbook.sheet_by_index(0)
-        #获取excel的行和列
-        nrows = sheet.nrows
-        ncols = sheet.ncols
-        print(ncols,nrows)
-        sql = "insert into working_hours (jobnum,name,workingtime,category,project,date,createtime) VALUES"
-        for i in range(1,nrows):
-            row = sheet.row_values(i)
-            jobnum = row[4]
-            name = row[5]
-            workingtime = row[2]
-            category = row[8]
-            project = row[1]
-            date = xldate_as_datetime(row[3],0).strftime('%Y/%m/%d')
-            values = "('%s','%s','%s','%s','%s','%s','%s')"%(jobnum,name,workingtime,category,project,date,datetime.datetime.now())
-            sql = sql + values +","
-        # 为了提高运行效率，一次性把数据 insert 进数据库  　
-        sql = sql[:-1]
-        # 写入数据库  
-        # DataConnection 是自定义的公共模块，用的是第三方库，用来操作数据库。没有用 ORM ，后续有 group by 等复杂 sql 不好操作。
-        DataConnection.MysqlConnection().insert('work',sql)
-
 
 def upload(request):
     # 根据name取 file 的值
@@ -152,7 +126,6 @@ def upload(request):
             for i in file.readlines():
                 f.write(i)
         ue = UploadExcelData(filename=file.name)
-        # ue.openvino_without_automl(1)
         ue.main()
     except Exception as e:
         return HttpResponse(e)
